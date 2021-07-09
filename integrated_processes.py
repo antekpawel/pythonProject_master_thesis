3abe5f3f0a169386927d81165503e42d79986e99
antekpawel <pawel.antkowiak.pw@gmail.com>
2021-07-09 23:07:22 +0200

    Adaptive time, problem with tanh-1

--- integrated_processes.py
+++ integrated_processes.py
@@ -2,7 +2,6 @@ import math
 import numpy as np
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
 import os
 from scipy.integrate import odeint
 
@@ -23,10 +22,9 @@ def r_reaction_cd(y, t, ca0, cb0, eq, rr):
 
 
 def pierwiastek(ca0, cb0, eq):
-    return math.sqrt(2 * ca0 * cb0 * eq ** 2
-                     - ca0 ** 2 * eq ** 2
-                     - cb0 ** 2 * eq ** 2
-                     - 4 * ca0 * cb0 * eq)
+    return math.sqrt(+ ca0 ** 2 * eq
+                     + cb0 ** 2 * eq
+                     - 2 * ca0 * cb0 * (eq - 2))
 
 
 def stezenie_row(ca0, cb0, tem):
@@ -43,50 +41,58 @@ try:
 except IOError:
     print("No file!")
 
-Temperature = data_IP['T_r [C]'][0]
-c_A0 = data_IP['c_A0 [mol/m3]'][0]
-c_B0 = data_IP['c_B0 [mol/m3]'][0]
+dataset = pd.DataFrame()
+plot = False
 
-k2 = reaction_rate(Temperature)
-Ke = equilibrium(Temperature)
+for index, row in data_IP.iterrows():
+    i = index
+    # Set input data
+    Temperature = data_IP['T_r [C]'][i]
+    c_A0 = data_IP['c_A0 [mol/m3]'][i]
+    c_B0 = data_IP['c_B0 [mol/m3]'][i]
+    k2 = reaction_rate(Temperature)
+    Ke = equilibrium(Temperature)
 
-# sqrt_val = pierwiastek(c_A0, c_B0, Ke)
-eq_concetration = stezenie_row(c_A0, c_B0, Temperature)
-print(eq_concetration)
-# numerator = - c_A0 * Ke - c_B0 * Ke
-# finish_time = 2 * Ke / sqrt_val * math.atan((numerator + 2 * (Ke - 1) * eq_concetration) / sqrt_val) / k2
-# print(finish_time)
+    sqrt_val = pierwiastek(c_A0, c_B0, Ke)
+    eq_concetration = stezenie_row(c_A0, c_B0, Temperature)
+    print(eq_concetration)
+    numerator = c_A0 * Ke + c_B0 * Ke
+    arctan = 1 - ((numerator - 2 * (Ke - 1) * eq_concetration * 0.95) / sqrt_val / math.sqrt(Ke) - 1)
+    arctan = 0.99
+    print(arctan)
+    finish_time = 2 * math.sqrt(Ke) / sqrt_val / k2 * math.log((1 + arctan) / (1 - arctan)) / 2
+    print(finish_time)
 
-reaction_time = np.linspace(0, 4 * 3600)
-c_C = odeint(r_reaction_cd, 0, reaction_time, args=(c_A0, c_B0, Ke, k2))
+    # Numerical output
+    reaction_time = np.linspace(0, finish_time)
+    c_C = odeint(r_reaction_cd, 0, reaction_time, args=(c_A0, c_B0, Ke, k2))
 
-# Table of output data
-con_in_time = pd.DataFrame(list(zip(reaction_time, np.hstack(c_C))), columns=['Czas [s]', 'c_C = c_D [mol/m3]'])
-con_in_time['c_A [mol/m3]'] = c_A0 - con_in_time['c_C = c_D [mol/m3]']
-con_in_time['c_B [mol/m3]'] = c_B0 - con_in_time['c_C = c_D [mol/m3]']
-con_in_time['r [mol/m3/s]'] = k2 * (con_in_time['c_A [mol/m3]'] * con_in_time['c_B [mol/m3]']
-                                    - con_in_time['c_C = c_D [mol/m3]'] ** 2 / Ke)
+    # Table of output data
+    con_in_time = pd.DataFrame(list(zip(np.hstack(c_C))), columns=['c_C = c_D [mol/m3]'])
+    con_in_time['c_A [mol/m3]'] = c_A0 - con_in_time['c_C = c_D [mol/m3]']
+    con_in_time['c_B [mol/m3]'] = c_B0 - con_in_time['c_C = c_D [mol/m3]']
+    con_in_time['Reaction temperature [C]'] = Temperature
+    con_in_time['r [mol/m3/s]'] = k2 * (con_in_time['c_A [mol/m3]'] * con_in_time['c_B [mol/m3]']
+                                        - con_in_time['c_C = c_D [mol/m3]'] ** 2 / Ke)
 
-print(con_in_time)
+    # Plot data
+    if plot:
+        fig_concetration = px.line(con_in_time,
+                                   x='Czas [s]',
+                                   y=['c_B [mol/m3]', 'c_A [mol/m3]', 'c_C = c_D [mol/m3]'],
+                                   labels=dict(x="Czas [s]", y="Stezenie [mol/m3]", color="Skladniki"))
+        fig_concetration.show()
+        fig_concetration.write_image('images/Concetration Tr ' + str(data_IP['T_r [C]'][i])
+                                     + ' c_A0 ' + str(data_IP['c_A0 [mol/m3]'][i])
+                                     + ' c_B0 ' + str(data_IP['c_B0 [mol/m3]'][i])
+                                     + '.svg')
 
-# Plot data
-fig_concetration = px.line(con_in_time,
-                           x='Czas [s]',
-                           y=['c_B [mol/m3]', 'c_A [mol/m3]', 'c_C = c_D [mol/m3]'],
-                           labels=dict(x="Czas [s]", y="Stezenie [mol/m3]", color="Skladniki"))
-fig_concetration.show()
-fig_concetration.write_image('images/Concetration Tr ' + str(data_IP['T_r [C]'][0])
-                             + ' c_A0 ' + str(data_IP['c_A0 [mol/m3]'][0])
-                             + ' c_B0 ' + str(data_IP['c_B0 [mol/m3]'][0])
-                             + '.svg')
+    # Check the value
+    print(c_C[-1] ** 2 / (c_A0 - c_C[-1]) / (c_B0 - c_C[-1]) / equilibrium(Temperature))
 
-# Check the value
-print(c_C[-1] ** 2 / (40 - c_C[-1]) / (20 - c_C[-1]) / equilibrium(20))
+    # Save data
+    dataset = pd.concat([dataset, con_in_time], ignore_index=True)
 
-# Save data
-dataset = pd.concat([dataSet, fluidProp], ignore_index=True)
+print(dataset)
 with pd.ExcelWriter('Concetration_data.xlsx') as writer:
-    dataset.to_excel(writer,
-                     sheet_name='Example Tr ' + str(data_IP['T_r [C]'][0])
-                                + ' c_A0 ' + str(data_IP['c_A0 [mol/m3]'][0])
-                                + ' c_B0 ' + str(data_IP['c_B0 [mol/m3]'][0]))
+    dataset.to_excel(writer, sheet_name='Concetration_data')
