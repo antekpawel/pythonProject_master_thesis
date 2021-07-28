5654a9cbfb963683a8709de58288974ae3bde907
antekpawel <pawel.antkowiak.pw@gmail.com>
2021-07-28 15:01:48 +0200

    Kolgomorov

--- Integreated_processes_NN.py
+++ Integreated_processes_NN.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 import os
 import re
@@ -19,10 +19,12 @@ def norm(x):
 
 def build_model():
     model = Sequential()
-    model.add(Dense(4, input_dim=2))
-    model.add(Dense(1, activation='relu'))
+    model.add(Dense(3, activation='sigmoid', input_dim=4))
+    model.add(Dense(3, activation='sigmoid'))
+    model.add(Dense(3, activation='relu'))
+    model.add(Dense(1))
 
-    model.compile(optimizer='SGD',
+    model.compile(optimizer='adam',
                   loss='mse',
                   metrics=['mse'])
     return model
@@ -49,30 +51,6 @@ def get_rmse(history):
     return hist['rmse']
 
 
-def get_ideal_gas_data():
-    np.random.seed(1)
-    pressure = np.random.rand(100) * 9.9 + 0.1
-    temperature = np.random.rand(100) * 200 + 273.15
-    density = pressure * 1e6 / temperature / 8.314 * 1 / 1000
-
-    w_p = (pressure - 0.1) / 9.9
-    w_t = (temperature - 273.15) / 200
-    d_min = 0.1 * 1e6 / 473.15 / 8.314 * 1 / 1000
-    d_max = 10 * 1e6 / 273.15 / 8.314 * 1 / 1000
-    w_d = (density - d_min) / (d_max - d_min)
-
-    for i in w_d:
-        if i > 1 or i < 0:
-            print('err')
-
-    ideal_gas_data_f = pd.DataFrame(list(zip(pressure, temperature, density, w_p, w_t, w_d)),
-                                    columns=['Pressure [MPa]', 'Temperature [K]',
-                                             'Density [kg/m3]', 'W_p [-]', 'W_t [-]', 'W_d [-]'])
-
-    ideal_gas_data_f.to_excel('Ideal_gas_data.xlsx')
-    return ideal_gas_data_f
-
-
 def save_figure(vector, name):
     fig_err_abs = go.Figure()
     fig_err_abs.add_trace(go.Scatter(y=vector, mode='markers'))
@@ -82,27 +60,29 @@ def save_figure(vector, name):
 
 
 try:
-    with open('Ideal_gas_data.xlsx', encoding='utf-8', errors='ignore') as f:
-        ideal_gas_data = pd.read_excel("Ideal_gas_data.xlsx", index_col=0)
+    with open('Concetration_data.xlsx', encoding='utf-8', errors='ignore') as f:
+        ideal_gas_data = pd.read_excel("Concetration_data.xlsx", index_col=0)
 except IOError:
-    ideal_gas_data = get_ideal_gas_data()
+    print('Error, no such file!')
 
 dataset = ideal_gas_data.copy()
 
-fig = px.scatter_matrix(dataset, dimensions=['Pressure [MPa]', 'Temperature [K]'], height=700)
-'''fig.show()
-fig.write_image("images/Relation_chart.svg")'''
+fig = px.scatter_matrix(dataset,
+                        dimensions=['c_C = c_D [mol/m3]', 'c_A [mol/m3]', 'c_B [mol/m3]', 'Reaction temperature [C]'],
+                        height=700)
+# fig.show()
+# fig.write_image("images/Relation_chart.svg")
 
-dataset = dataset[['Pressure [MPa]', 'Temperature [K]', 'Density [kg/m3]']]
+# dataset = dataset[['Pressure [MPa]', 'Temperature [K]', 'Density [kg/m3]']]
 train_dataset = dataset.sample(frac=0.7, random_state=0)
 test_dataset = dataset.drop(train_dataset.index)
 
 train_stats = train_dataset.describe()
-train_stats.pop('Density [kg/m3]')
+train_stats.pop('r [mol/m3/s]')
 train_stats = train_stats.transpose()
 
-train_labels = train_dataset.pop('Density [kg/m3]')
-test_labels = test_dataset.pop('Density [kg/m3]')
+train_labels = train_dataset.pop('r [mol/m3/s]')
+test_labels = test_dataset.pop('r [mol/m3/s]')
 
 normed_train_data = norm(train_dataset)
 normed_test_data = norm(test_dataset)
@@ -112,28 +92,28 @@ normed_train_data = normed_train_data.values
 
 filepath = 'Best_weights.hdf5'
 checkpoint = ModelCheckpoint(filepath=filepath, monitor='mse', verbose=0, save_best_only=True, mode='min')
-es = EarlyStopping(monitor='mse', mode='min', verbose=1, patience=8)
+es = EarlyStopping(monitor='mse', mode='min', verbose=1, patience=15)
 
 model = build_model()
 history = model.fit(normed_train_data, train_labels.values,
-                    epochs=500,
+                    epochs=1000,
                     validation_split=0.2,
-                    verbose=0,
+                    verbose=1,
                     batch_size=32,
-                    callbacks=[checkpoint, es])
+                    callbacks=[es, checkpoint])
 plot_hist(history)
 print(model.summary())
 
 test_predictions = model.predict(normed_test_data).flatten()
 data_compare = pd.DataFrame(test_labels)
-data_compare['Predicted density [kg/m3]'] = test_predictions
-data_compare['Error [kg/m3]'] = abs(test_labels - test_predictions)
-data_compare['Error [%]'] = data_compare['Error [kg/m3]'] / test_labels
+data_compare['Predicted r [mol/m3/s]'] = test_predictions
+data_compare['Error [mol/m3/s]'] = abs(test_labels - test_predictions)
+data_compare['Error [%]'] = data_compare['Error [mol/m3/s]'] / test_labels
 data_compare = data_compare.reset_index(drop=True)
-data_compare = data_compare.sort_values(by="Density [kg/m3]", ignore_index=True)
+data_compare = data_compare.sort_values(by="r [mol/m3/s]", ignore_index=True)
 
 save_figure(data_compare['Error [%]'], 'Error [%]')
-save_figure(data_compare['Error [kg/m3]'], 'Error [kg/m^3]')
+# save_figure(data_compare['Error [mol/m3/s]'], 'Error [mol/m3/s]')
 
 rmse = get_rmse(history)
 r2 = pd.DataFrame({'r2': [r2_score(test_labels, test_predictions)]})
